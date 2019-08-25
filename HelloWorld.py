9e5bf1488f53612f3f1540d760e1b68168ce216f
Maxim <maxim.marchal@hotmail.com>
2019-08-25 20:54:52 +0200

    Moved files to crawl_util

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
-from main import crawl_endpoints, discover_links_from_endpoint
+from crawl_util.main import crawl_endpoints, discover_links_from_endpoint
 
 links = discover_links_from_endpoint('','kuipris.nl')
 crawl_endpoints(links, max_pages=20)
