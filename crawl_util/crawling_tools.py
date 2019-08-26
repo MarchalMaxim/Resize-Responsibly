5ce6be143ded719f086482eb1f361d2f3631d50c
Maxim <maxim.marchal@hotmail.com>
2019-08-26 20:50:13 +0200

    Made a function for checking if we want to visit an URL.

--- crawl_util/crawling_tools.py
+++ crawl_util/crawling_tools.py
@@ -7,20 +7,20 @@ from crawl_util.load_config import get_window_sizes, get_ignored_extensions
 driver_path = 'chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.headless = True
-browser = webdriver.Chrome(driver_path,options=options)
-ignored_extensions = ['.pdf', '.png', '.jpg', '.jpeg', '.docx']
+browser = webdriver.Chrome(driver_path, options=options)
 t = time.localtime()
 container_directory = 'crawl{}_{}_{}'.format(t.tm_hour, t.tm_min, t.tm_sec)
 os.mkdir(container_directory, 0o755)
 for device_name in get_window_sizes().keys():
     os.mkdir(container_directory + '/' + device_name, 0o755)
 
-
-def discover_links_from_endpoint(endpoint, ROOT_DOMAIN="kuipris.nl"):
+def discover_links_from_endpoint(endpoint, ROOT_DOMAIN="google.com"):
     if endpoint is None:
         return []
-    full_url = 'https://{}/{}'.format(ROOT_DOMAIN, urlparse(endpoint).path)
-    browser.get(full_url)
+    full_url = urlparse('https://{}/{}'.format(ROOT_DOMAIN, urlparse(endpoint).path))
+    if not _validate_url(full_url):
+        return []
+    browser.get(full_url.geturl())
     anchors = browser.find_elements_by_tag_name('a')
     links = list(map(lambda link: link.get_attribute('href'), anchors))
     # returns endpoints that are in the root domain
@@ -30,8 +30,18 @@ def discover_links_from_endpoint(endpoint, ROOT_DOMAIN="kuipris.nl"):
             )
     return list(links_from_domain)
 
+def _validate_url(full_url):
+    # full_url is a parsed url
+    if any([full_url.path.endswith(ext) for ext in get_ignored_extensions()]):
+        print('Skipped page with ignored extension: ' + full_url.path)
+        return False
+    if full_url.scheme == 'mailto':
+        print('Skipped mailto: ' + full_url.geturl())
+        return False
+    return True
 
-def crawl_endpoints(endpoints, base_screenshot_dir="", ROOT_DOMAIN='https://kuipris.nl/',  max_pages=10):
+
+def crawl_endpoints(endpoints, base_screenshot_dir="", ROOT_DOMAIN='google.com',  max_pages=10):
     crawled = 0
     for index, ep in enumerate(endpoints):
         full_url = urlparse(ep)
@@ -40,13 +50,8 @@ def crawl_endpoints(endpoints, base_screenshot_dir="", ROOT_DOMAIN='https://kuip
             break
 
         # Deciding whether we want to visit this page or not:
-        if any([full_url.path.endswith(ext) for ext in get_ignored_extensions()]):
-            print('Skipped page with ignored extension: ' + full_url.path)
-
-        if full_url.scheme == 'mailto':
-            print('Skipped mailto: '+full_url.geturl())
+        if not _validate_url(full_url):
             continue
-        print('visiting: '+full_url.geturl())
         browser.get(full_url.geturl())
         for device in get_window_sizes().items():
             device_name, size = device
